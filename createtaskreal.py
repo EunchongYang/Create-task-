b1113b7cad3b979184770007ba58f24dfe0e6004
EunchongYang <144172210+EunchongYang@users.noreply.github.com>
2024-04-18 13:33:01 -0700

    Update createtaskreal.py

--- createtaskreal.py
+++ createtaskreal.py
@@ -1,1 +1,54 @@
 
+
+classschedule = {}
+
+
+def schedule_yourclass():
+    classname = input("Enter your class name: ")
+    day = input("What day(s) do you have this class (e.g., Tuesday): ")
+    time = input("Enter the time (e.g., 11:00 AM): ")
+    classschedule[classname] = [day, time]
+    print("A new class is added to your schedule " + classname + " on " + day + " during " + time)
+
+
+def delete_class():
+    classname = input("What class would you like to delete? ")
+    if classname in classschedule:
+        del classschedule[classname]
+        print("Your class " + classname + " has been deleted")
+    else:
+        print("Class name was not found in the schedule. Try again")
+
+
+def view_schedule():
+    for key in classschedule:
+        print("Your class", key, "is on", classschedule[key][0], "at", classschedule[key][1])
+
+
+def manage_schedule():
+    choice = "1"
+    while choice != "4":
+        print("Class scheduler")
+        print("1. Add a class")
+        print("2. Delete a class")
+        print("3. View class schedule")
+        print("4. Quit")
+
+
+        choice = input("Enter your choice (1/2/3/4): ")
+
+
+        if choice == '1':
+            schedule_yourclass()
+        elif choice == '2':
+            delete_class()
+        elif choice == '3':
+            view_schedule()
+        elif choice == '4':
+            print("Thank you for using the class scheduler. Have a good day!")
+        else:
+            print("Invalid choice. Please try again.")
+
+
+
+manage_schedule()
